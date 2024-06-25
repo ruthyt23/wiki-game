278e3f88126e427d1c7c54c72ad50631d0310236
Ruth Taddesse <ruth.y.taddesse@gmail.com>
2024-06-25 19:40:14 +0000

    Interstate map functions

--- src/interstate.ml
+++ src/interstate.ml
@@ -1,4 +1,55 @@
 open! Core
+module City = String
+
+module Interstate = struct
+  module Connection = struct
+    module T = struct
+      type t = City.t * City.t [@@deriving compare, sexp]
+    end
+
+    include Comparable.Make (T)
+  end
+
+  type t = Connection.Set.t [@@deriving sexp_of]
+
+  let no_periods city =
+    String.split_on_chars city ~on:[ ' ' ]
+    |> String.concat ~sep:"_"
+    |> String.split_on_chars ~on:[ '.' ]
+    |> String.concat
+  ;;
+
+  let rec pairs_from_interstate city_list : (string * string) list =
+    match city_list with
+    | [] -> []
+    | [ city ] ->
+      ignore city;
+      []
+    | [ city1; city2 ] -> [ city1, city2 ]
+    | city1 :: cities ->
+      List.map cities ~f:(fun x -> city1, x) @ pairs_from_interstate cities
+  ;;
+
+  let connections_from_highway (interstate : string) =
+    match String.split ~on:',' interstate with
+    | _ :: tail -> List.map ~f:no_periods tail |> pairs_from_interstate
+    | _ -> []
+  ;;
+
+  let of_file input_file =
+    print_endline (no_periods "St. Louis");
+    print_endline (no_periods "Washington D.C.");
+    print_endline (no_periods "Terre Haute");
+    let interstate_system_lines =
+      In_channel.read_lines (File_path.to_string input_file)
+    in
+    let interstate_system =
+      List.concat_map interstate_system_lines ~f:(fun x ->
+        connections_from_highway x)
+    in
+    Connection.Set.of_list interstate_system
+  ;;
+end
 
 let load_command =
   let open Command.Let_syntax in
@@ -9,25 +60,42 @@ let load_command =
         flag
           "input"
           (required File_path.arg_type)
-          ~doc:"FILE a file listing interstates and the cities they go through"
+          ~doc:
+            "FILE a file listing interstates and the cities they go through"
       in
       fun () ->
-        ignore (input_file : File_path.t);
-        failwith "TODO"]
+        let interstate_system = Interstate.of_file input_file in
+        printf !"%{sexp: Interstate.t}\n" interstate_system]
 ;;
+
+module G = Graph.Imperative.Graph.Concrete (City)
+
+module Dot = Graph.Graphviz.Dot (struct
+    include G
+
+    let edge_attributes _ = [ `Dir `None ]
+    let default_edge_attributes _ = []
+    let get_subgraph _ = None
+    let vertex_attributes v = [ `Shape `Box; `Label v; `Fillcolor 1000 ]
+    let vertex_name v = v
+    let default_vertex_attributes _ = []
+    let graph_attributes _ = []
+  end)
 
 let visualize_command =
   let open Command.Let_syntax in
   Command.basic
     ~summary:
-      "parse a file listing interstates and generate a graph visualizing the highway \
-       network"
+      "parse a file listing interstates and generate a graph visualizing \
+       the highway network"
     [%map_open
       let input_file =
         flag
           "input"
           (required File_path.arg_type)
-          ~doc:"FILE a file listing all interstates and the cities they go through"
+          ~doc:
+            "FILE a file listing all interstates and the cities they go \
+             through"
       and output_file =
         flag
           "output"
@@ -35,8 +103,13 @@ let visualize_command =
           ~doc:"FILE where to write generated graph"
       in
       fun () ->
-        ignore (input_file : File_path.t);
-        ignore (output_file : File_path.t);
+        let interstate_system = Interstate.of_file input_file in
+        let graph = G.create () in
+        Set.iter interstate_system ~f:(fun (city1, city2) ->
+          G.add_edge graph city1 city2);
+        Dot.output_graph
+          (Out_channel.create (File_path.to_string output_file))
+          graph;
         printf !"Done! Wrote dot file to %{File_path}\n%!" output_file]
 ;;
 
