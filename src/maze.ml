124c16f879a6e98539566bd3c27a0fb991f1ed5a
Ruth Taddesse <ruth.y.taddesse@gmail.com>
2024-06-27 15:37:57 +0000

    Graph search #3: maze solver

--- src/maze.ml
+++ src/maze.ml
@@ -1,16 +1,113 @@
 open! Core
 
+type t =
+  | None
+  | Wall
+  | Start
+  | End
+[@@deriving compare, sexp, hash]
+
+let position_to_string (position : int * int) : string =
+  let x, y = position in
+  Int.to_string x ^ ", " ^ Int.to_string y
+;;
+
+let position_val ~maze_array ~row ~col : char =
+  List.nth_exn (List.nth_exn maze_array row) col
+;;
+
+let txt_to_array resource : char list list =
+  let contents = In_channel.read_lines (File_path.to_string resource) in
+  List.map contents ~f:(fun x -> String.to_list x)
+;;
+
+let array_height maze_array = List.length maze_array
+let array_length maze_array = List.length (List.nth_exn maze_array 0)
+
+let find_start_pos maze_array =
+  let found = ref false in
+  let row = ref 0 in
+  let col = ref 0 in
+  while not found.contents do
+    if Char.equal
+         (position_val ~maze_array ~row:row.contents ~col:col.contents)
+         'S'
+    then found.contents <- true
+    else (
+      col.contents <- col.contents + 1;
+      if col.contents = array_length maze_array
+      then (
+        col.contents <- 0;
+        row.contents <- row.contents + 1)
+      else ())
+  done;
+  !row, !col
+;;
+
+let find_neighbors ~(maze_array : char list list) ~position ~length ~height
+  : (int * int) list
+  =
+  let (x : int), (y : int) = position in
+  let neighbor_list = [ x + 1, y; x - 1, y; x, y + 1; x, y - 1 ] in
+  List.filter neighbor_list ~f:(fun (a, b) ->
+    a >= 0 && a < height && b >= 0 && b < length)
+  |> List.filter ~f:(fun (a, b) ->
+    not (Char.equal (position_val ~maze_array ~row:a ~col:b) '#'))
+;;
+
+let rec solve
+  ~(maze_array : char list list)
+  ~position
+  ~length
+  ~height
+  ~visited
+  : (int * int) list
+  =
+  let (row : int), (col : int) = position in
+  if Char.equal (position_val ~maze_array ~row ~col) 'E'
+  then [ position ]
+  else (
+    let neighbors = find_neighbors ~maze_array ~position ~length ~height in
+    if List.is_empty neighbors
+    then []
+    else
+      List.concat_map neighbors ~f:(fun neighbor ->
+        match
+          Hash_set.find visited ~f:(fun y ->
+            String.equal (position_to_string neighbor) y)
+        with
+        | Some _ -> []
+        | _ ->
+          Hash_set.add visited (position_to_string neighbor);
+          let neighbor_path =
+            solve ~maze_array ~position:neighbor ~length ~height ~visited
+          in
+          (match neighbor_path with
+           | [] -> []
+           | _ -> [ position ] @ neighbor_path)))
+;;
+
 let solve_command =
   let open Command.Let_syntax in
   Command.basic
     ~summary:"parse a file containing a maze and find a solution"
     [%map_open
       let input_file =
-        flag "input" (required File_path.arg_type) ~doc:"FILE a file containing a maze"
+        flag
+          "input"
+          (required File_path.arg_type)
+          ~doc:"FILE a file containing a maze"
       in
       fun () ->
-        ignore (input_file : File_path.t);
-        failwith "TODO"]
+        let maze_array = txt_to_array input_file in
+        let height = array_height maze_array in
+        let length = array_length maze_array in
+        let start_pos = find_start_pos maze_array in
+        let visited = Hash_set.create (module String) in
+        solve ~maze_array ~position:start_pos ~length ~height ~visited
+        |> List.iter ~f:(fun x -> print_endline (position_to_string x))]
 ;;
 
-let command = Command.group ~summary:"maze commands" [ "solve", solve_command ]
+let command =
+  Command.group ~summary:"maze commands" [ "solve", solve_command ]
+;;
--- src/wiki_game.ml
+++ src/wiki_game.ml
@@ -2,17 +2,18 @@ open! Core
 
 module Article = struct
   module T = struct
-    type t = {
-  url : string;
-  title : string; 
-  } 
-  [@@deriving compare, sexp, hash]
+    type t =
+      { url : string
+      ; title : string
+      }
+    [@@deriving compare, sexp, hash]
   end
-  include Comparable.Make (T)
-include T
 
-let create title url = {title ; url}
-let title t = t.title
+  include Comparable.Make (T)
+  include T
+
+  let create title url = { title; url }
+  let title t = t.title
 end
 
 module G = Graph.Imperative.Graph.Concrete (String)
@@ -29,29 +30,16 @@ module Dot = Graph.Graphviz.Dot (struct
     let graph_attributes _ = []
   end)
 
-(* [get_linked_articles] should return a list of wikipedia article lengths contained in
-   the input.
-
-   Note that [get_linked_articles] should ONLY return things that look like wikipedia
-   articles. In particular, we should discard links that are:
-   - Wikipedia pages under special namespaces that are not articles (see
-     https://en.wikipedia.org/wiki/Wikipedia:Namespaces)
-   - other Wikipedia internal URLs that are not articles
-   - resources that are external to Wikipedia
-   - page headers
-
-   One nice think about Wikipedia is that stringent content moderation results in
-   uniformity in article format. We can expect that all Wikipedia article links parsed
-   from a Wikipedia page will have the form "/wiki/<TITLE>". *)
 let get_linked_articles contents : string list =
   let open Soup in
   parse contents
   $$ "a[href*='/wiki/']"
   |> to_list
-  |> List.filter_map ~f:(fun x -> (match Wikipedia_namespace.namespace (R.attribute "href" x) with
+  |> List.filter_map ~f:(fun x ->
+    match Wikipedia_namespace.namespace (R.attribute "href" x) with
     | None -> Some (R.attribute "href" x)
-    | Some _special -> None))
-  |> List.dedup_and_sort ~compare:(String.compare)
+    | Some _special -> None)
+  |> List.dedup_and_sort ~compare:String.compare
 ;;
 
 let print_links_command =
@@ -65,50 +53,79 @@ let print_links_command =
         List.iter (get_linked_articles contents) ~f:print_endline]
 ;;
 
-(* [visualize] should explore all linked articles up to a distance of [max_depth] away
-   from the given [origin] article, and output the result as a DOT file. It should use the
-   [how_to_fetch] argument along with [File_fetcher] to fetch the articles so that the
-   implementation can be tested locally on the small dataset in the ../resources/wiki
-   directory. *)
+(* [visualize] should explore all linked articles up to a distance of
+   [max_depth] away from the given [origin] article, and output the result as
+   a DOT file. It should use the [how_to_fetch] argument along with
+   [File_fetcher] to fetch the articles so that the implementation can be
+   tested locally on the small dataset in the ../resources/wiki directory. *)
 
-let no_parens text = String.split_on_chars text ~on:[ ' ' ]
-|> String.concat ~sep:""
-|> String.split_on_chars ~on:[ '(' ]
+let no_parens text =
+  String.split_on_chars text ~on:[ ' ' ] (* String.substr_replace_all *)
+  |> String.concat ~sep:""
+  |> String.split_on_chars ~on:[ '(' ]
   |> String.concat ~sep:"_"
   |> String.split_on_chars ~on:[ ')' ]
   |> String.concat
 ;;
 
-let article_tuples link_prefix article1 article2 = (Article.create (String.chop_prefix_exn article1 ~prefix:"/wiki/") (link_prefix ^ article1), Article.create (String.chop_prefix_exn article2 ~prefix:"/wiki/") (link_prefix ^ article2))
+let article_tuples link_prefix article1 article2 =
+  ( Article.create
+      (String.chop_prefix_exn article1 ~prefix:"/wiki/")
+      (link_prefix ^ article1)
+  , Article.create
+      (String.chop_prefix_exn article2 ~prefix:"/wiki/")
+      (link_prefix ^ article2) )
+;;
 
-let rec wiki_explore ~max_depth ~origin ~how_to_fetch : (Article.t * Article.t) list = 
+let rec wiki_explore ~max_depth ~origin ~how_to_fetch
+  : (Article.t * Article.t) list
+  =
   if max_depth = 0
-    then []
+  then []
   else (
-  let contents = get_linked_articles (File_fetcher.fetch_exn how_to_fetch ~resource:origin) in
-  let to_visit = Hash_set.create (module String) in
-  let link_prefix = match how_to_fetch with 
-  | Local root -> ignore root; ""
-  | Remote -> "https://en.wikipedia.org" in
-  let current_map = List.map contents ~f:(fun x -> Hash_set.add to_visit x; article_tuples link_prefix origin x) in 
-  current_map @ List.concat_map (Hash_set.to_list to_visit) ~f:(fun x -> wiki_explore ~max_depth:(max_depth - 1) ~origin:x ~how_to_fetch)
-  )
+    let contents =
+      get_linked_articles
+        (File_fetcher.fetch_exn how_to_fetch ~resource:origin)
+    in
+    let to_visit = Hash_set.create (module String) in
+    let link_prefix =
+      match how_to_fetch with
+      | Local root ->
+        ignore root;
+        ""
+      | Remote -> "https://en.wikipedia.org"
+    in
+    let current_map =
+      List.map contents ~f:(fun x ->
+        Hash_set.add to_visit x;
+        article_tuples link_prefix origin x)
+    in
+    current_map
+    @ List.concat_map (Hash_set.to_list to_visit) ~f:(fun x ->
+      wiki_explore ~max_depth:(max_depth - 1) ~origin:x ~how_to_fetch))
+;;
 
-  let visualize ?(max_depth = 3) ~origin ~output_file ~how_to_fetch (): unit =
-    let wiki_network = (wiki_explore ~max_depth ~origin:("/" ^ origin) ~how_to_fetch) in
-    let graph = G.create () in
-    List.iter wiki_network ~f:(fun (article1, article2) -> G.add_edge graph (no_parens (Article.title article1)) (no_parens (Article.title article2)));
-    Dot.output_graph
-            (Out_channel.create (File_path.to_string output_file))
-            graph;
-  ;;
+let visualize ?(max_depth = 3) ~origin ~output_file ~how_to_fetch () : unit =
+  let wiki_network =
+    wiki_explore ~max_depth ~origin:("/" ^ origin) ~how_to_fetch
+  in
+  let graph = G.create () in
+  List.iter wiki_network ~f:(fun (article1, article2) ->
+    G.add_edge
+      graph
+      (no_parens (Article.title article1))
+      (no_parens (Article.title article2)));
+  Dot.output_graph
+    (Out_channel.create (File_path.to_string output_file))
+    graph
+;;
 
 let visualize_command =
   let open Command.Let_syntax in
   Command.basic
     ~summary:
-      "parse a file listing interstates and generate a graph visualizing the highway \
-       network"
+      "parse a file listing interstates and generate a graph visualizing \
+       the highway network"
     [%map_open
       let how_to_fetch = File_fetcher.How_to_fetch.param
       and origin = flag "origin" (required string) ~doc:" the starting page"
@@ -128,14 +145,15 @@ let visualize_command =
         printf !"Done! Wrote dot file to %{File_path}\n%!" output_file]
 ;;
 
-(* [find_path] should attempt to find a path between the origin article and the
-   destination article via linked articles.
+(* [find_path] should attempt to find a path between the origin article and
+   the destination article via linked articles.
 
-   [find_path] should use the [how_to_fetch] argument along with [File_fetcher] to fetch
-   the articles so that the implementation can be tested locally on the small dataset in
-   the ../resources/wiki directory.
+   [find_path] should use the [how_to_fetch] argument along with
+   [File_fetcher] to fetch the articles so that the implementation can be
+   tested locally on the small dataset in the ../resources/wiki directory.
 
-   [max_depth] is useful to limit the time the program spends exploring the graph. *)
+   [max_depth] is useful to limit the time the program spends exploring the
+   graph. *)
 let find_path ?(max_depth = 3) ~origin ~destination ~how_to_fetch () =
   ignore (max_depth : int);
   ignore (origin : string);
@@ -147,11 +165,14 @@ let find_path ?(max_depth = 3) ~origin ~destination ~how_to_fetch () =
 let find_path_command =
   let open Command.Let_syntax in
   Command.basic
-    ~summary:"Play wiki game by finding a link between the origin and destination pages"
+    ~summary:
+      "Play wiki game by finding a link between the origin and destination \
+       pages"
     [%map_open
       let how_to_fetch = File_fetcher.How_to_fetch.param
       and origin = flag "origin" (required string) ~doc:" the starting page"
-      and destination = flag "destination" (required string) ~doc:" the destination page"
+      and destination =
+        flag "destination" (required string) ~doc:" the destination page"
       and max_depth =
         flag
           "max-depth"
